d23a27b5681d994e58f24f1945a4a4c899728e97
anagamin <anagamin@yahoo-corp.jp>
2022-07-21 23:25:06 +0900

    github pages

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
   },
   root: './src', //開発ディレクトリ設定
   build: {
-    base: './', //相対パスでビルドする
+    base: '/vite-vanilla/', //gitのリポジトリ
     outDir: '../dist', //出力場所の指定
     rollupOptions: {
       //ファイル出力設定
